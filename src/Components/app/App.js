51f2e5f0cf0ccd63fe5953d1141bcde8b386826c
percworld <percworld@gmail.com>
2022-03-10 16:50:59 -0700

    toLowercase filenames for linux base

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,42 +1,47 @@
 //import { Route, Switch } from 'react-router-dom';
+import React from 'react';
 import './App.scss';
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
+import Header from '../header/header';
+import Footer from '../footer/footer';
 import Inanima from '../pics/Inanima.png';
 
-function App() {
-  return (
-    <main className="App">
-      <Header></Header>
-      <section className="body">
-        <img src={Inanima} className="artwork" alt="artwork" />
-        <div className="title-box">
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <main className="App">
+        <Header></Header>
+        <section className="body">
+          <img src={Inanima} className="artwork" alt="artwork" />
+          <div className="title-box">
 
-          <p className="title">
-            PHIBBER
-            {/* Chuck Morris - new app boiler */}
-          </p>
-        </div>
-        <div className="login-or-link">
-          <a
-            className="App-link"
-            href="soundcloud.com/phibber"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Whistle While you Work
-          </a>
-        </div>
-        <div className="petal">
+            <p className="title">
+              PHIBBER
+              {/* Chuck Morris - new app boiler */}
+            </p>
+          </div>
+          <div className="login-or-link">
+            <a
+              className="App-link"
+              href="soundcloud.com/phibber"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Whistle While you Work
+            </a>
+          </div>
+          <div className="petal">
 
-          <p className="title">
-            Init commit
-          </p>
-        </div>
-      </section>
-      <Footer></Footer>
-    </main>
-  );
+            <p className="title">
+              Init commit
+            </p>
+          </div>
+        </section>
+        <Footer></Footer>
+      </main>
+    );
+  }
 }
-
 export default App;
